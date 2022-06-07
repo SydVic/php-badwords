16a1779bab806123e1e13189ec89602b1f4c0390
SydVic <fabrizio.bocca27@gmail.com>
2022-06-07 14:50:21 +0200

    aggiunto get per badword e stampa testo censurato e lunghezza

--- index.php
+++ index.php
@@ -8,28 +8,52 @@
 </head>
 <body>
 
-<?php 
-$paragraph = "The ice age is coming, the sun's zooming in
-Engines stop running, the wheat is growing thin
-A nuclear era, but I have no fear
-'Cause London is drowning
-I, I live by the river.";
-?>
-
-<h2>Testo originale</h2>
-<p>
-  <?php
-  echo $paragraph;
-  ?>
-</p>
-
-<h2>Lunghezza del testo originale</h2>
-<p>
   <?php 
-  $paragraph_length = strlen($paragraph);
-  echo "Il testo originale è lungo " . $paragraph_length . " caratteri";
+  // la variabile contenente il paragrafo
+  $paragraph = "The ice age is coming, the sun's zooming in
+  Engines stop running, the wheat is growing thin
+  A nuclear era, but I have no fear
+  'Cause London is drowning
+  I, I live by the river.";
   ?>
-</p>
+
+  <h2>Testo originale</h2>
+  <p>
+    <?php
+    // stampa in html del paragrafo originale
+    echo $paragraph;
+    ?>
+  </p>
+
+  <h2>Lunghezza del testo originale</h2>
+  <p>
+    <?php
+    // stampa in html della lunghezza del paragrafo originale
+    $paragraph_length = strlen($paragraph);
+    echo "Il testo originale è lungo " . $paragraph_length . " caratteri";
+    ?>
+  </p>
+
+  <?php
+  $user_badword = $_GET['badword'];
+  ?>
+
+  <h2>Paragrafo censurato</h2>
+  <p>
+    <?php
+    $censored_paragraph = str_replace($user_badword, "***", $paragraph);
+    echo $censored_paragraph;
+    ?>
+  </p>
+
+  <h2>Lunghezza del testo censurato</h2>
+  <p>
+    <?php
+    // stampa in html della lunghezza del paragrafo originale
+    $censored_paragraph_length = strlen($censored_paragraph);
+    echo "Il testo censurato è lungo " . $censored_paragraph_length . " caratteri";
+    ?>
+  </p>
 
 </body>
 </html>
