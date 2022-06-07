ba7ba1ca63adfab096662438d15034a3d20e178d
SydVic <fabrizio.bocca27@gmail.com>
2022-06-07 14:15:24 +0200

    aggiunta lunghezza stringa

--- index.php
+++ index.php
@@ -7,10 +7,29 @@
   <title>PHP Badwords</title>
 </head>
 <body>
-<h1>
-  <?php 
-  echo "prova";
+
+<?php 
+$paragraph = "The ice age is coming, the sun's zooming in
+Engines stop running, the wheat is growing thin
+A nuclear era, but I have no fear
+'Cause London is drowning
+I, I live by the river.";
+?>
+
+<h2>Testo originale</h2>
+<p>
+  <?php
+  echo $paragraph;
   ?>
-</h1>
+</p>
+
+<h2>Lunghezza del testo originale</h2>
+<p>
+  <?php 
+  $paragraph_length = strlen($paragraph);
+  echo "Il testo originale è lungo " . $paragraph_length . " caratteri";
+  ?>
+</p>
+
 </body>
 </html>
